1cc93237c8e0b14e18738170c20f2f79aeb8b20a
fabeto10 <f0414475@gmail.com>
2022-07-23 20:16:03 +0000

    branch:devUser: Elaboracion de Sign up, Log In y metodo de autenticacion jwt branch

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        <div className="ml-auto">
+          {/* renderizado condicional cuando esta iniciada la sesion */}
+          {!store.token ? (
+            <Link to="/sign-up">
+              <button className="btn btn-primary me-3">Sign Up</button>
+            </Link>
+          ) : (
+            <></>
+          )}
+          {!store.token ? (
+            <Link to="/login">
+              <button className="btn btn-primary">Log In</button>
+            </Link>
+          ) : (
+            <button
+              onClick={() => actions.logout()}
+              className="btn btn-primary"
+            >
+              Log Out
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,37 +1,37 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { SignUp } from "./pages/SignUp";
+import { Home } from "./pages/Home";
 import injectContext from "./store/appContext";
-
+import { Private } from "./pages/Private";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Login } from "./pages/Login.js";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Private />} path="/private" />
+            <Route element={<Home />} path="/" />
+            <Route element={<Login />} path="/log-in" />
+            <Route element={<SignUp />} path="/sign-up" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
