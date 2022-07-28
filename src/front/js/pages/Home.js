75d52bfe0e01945e88bf5d6ea3475d89c1271cb7
jerajbe <jerajbe@gmail.com>
2022-07-28 23:23:42 +0000

    Estilos a Home

--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -2,7 +2,23 @@ import React from "react";
 
 export const Home = () => {
   return (
-    <div className="fondo">
-    </div>
+    <>
+      <div className="header fondo">
+        <div className="row1">
+          <h1>Welcome to Lectur's Club</h1>
+        </div>
+        <div className="row2">
+          <h2>Find your Book</h2>
+          <div className="inputSearch">
+            <input type="text" placeholder="Enter your Book Name" />
+            <button>
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+          <img src="https://img.freepik.com/vector-premium/libreria-biblioteca-libreria-bestseller-estilo-dibujos-animados-ilustracion-vectorial_499431-682.jpg" />
+        </div>
+      </div>
+      <div className="container"></div>
+    </>
   );
 };
