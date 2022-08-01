4df7c924fb83cd2cb3c052a9d5c416021391c502
fabeto10 <f0414475@gmail.com>
2022-08-01 14:33:51 +0000

    se arreglo token

--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -8,7 +8,9 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
   useEffect(() => {
     actions.carouselBook();
-  }, []);
+    if (store.token && store.token != "" && store.token != undefined)
+      actions.getMessage();
+  }, [store.token]);
 
   const searchBook = (e) => {
     if (e.key == "Enter") {
