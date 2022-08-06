7435f6981c25b9f8bd1ff24d7ba6f48ca30041cc
fabeto10 <f0414475@gmail.com>
2022-08-06 21:31:06 +0000

    fix

--- src/front/js/pages/SingleBook.js
+++ src/front/js/pages/SingleBook.js
@@ -84,7 +84,7 @@ export const SingleBook = (props) => {
                   className="card mb-3 border border-0"
                   style={{ maxWwidth: "540px" }}
                 >
-                  <div className="row border border-0">
+                  <div className="row">
                     <div className="col-md-5">
                       <img
                         src={props.cover}
@@ -92,47 +92,27 @@ export const SingleBook = (props) => {
                         className="img-fluid w-100 h-100"
                       />
                     </div>
-                    <div className="col-md-7 overflow-auto">
-                      <div className="card-body border border-0 overflow-auto">
+                    <div className="col-md-7">
+                      <div className="card-body">
                         <h5 className="card-title">{`Authors: ${props.authors}`}</h5>
                         <p className="card-text">{`Year: ${props.year}`}</p>
-                        <p className="card-text">{`Language: ${props.language}`}</p>
-                        <p className="card-text">{props.description}</p>
+                        <p className="card-text">{`Language: ${props.rating}`}</p>
+                        <p
+                          className="card-text"
+                          style={{
+                            overflowY: "auto",
+                            width: "400px",
+                            height: "500px",
+                            padding: "1%",
+                          }}
+                        >
+                          {props.description}
+                        </p>
                       </div>
-
-                      <div
-                        className="card mb-3 border border-0"
-                        style={{ maxWwidth: "540px" }}
-                      >
-                        <div className="row">
-                          <div className="col-md-5">
-                            <img
-                              src={props.cover}
-                              alt={props.name}
-                              className="img-fluid w-100 h-100"
-                            />
-                          </div>
-                          <div className="col-md-7">
-                            <div className="card-body">
-                              <h5 className="card-title">{`Authors: ${props.authors}`}</h5>
-                              <p className="card-text">{`Year: ${props.year}`}</p>
-                              <p className="card-text">{`Language: ${props.rating}`}</p>
-                              <p
-                                className="card-text"
-                                style={{
-                                  overflowY: "auto",
-                                  width: "400px",
-                                  height: "500px",
-                                  padding: "1%",
-                                }}
-                              >
-                                {props.description}
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-
-                        {/*<div className="card mb-3" style={{ maxWidth: "540px" }}>
+                    </div>
+                  </div>
+                </div>
+                {/*<div className="card mb-3" style={{ maxWidth: "540px" }}>
                   <div className="row g-0">
                     <div className="col-md-4">
                       <img
@@ -155,60 +135,52 @@ export const SingleBook = (props) => {
                     </div>
                   </div>
                 </div>*/}
-
-                        {/* {aqui termina la info del libro modal} */}
-                        <div className="container bootdey">
-                          <div className="col-md-12 bootstrap snippets">
-                            <div className="panel">
-                              <div className="panel-body">
-                                <textarea
-                                  value={comment}
-                                  onChange={(e) => setComment(e.target.value)}
-                                  className="form-control"
-                                  rows="2"
-                                  placeholder="What are you thinking?"
-                                ></textarea>
-                                <div className="mar-top clearfix">
-                                  <button
-                                    onClick={() =>
-                                      actions.addComment({
-                                        google_books_id: props.google_books_id,
-                                        // book_id: commentBody.book_id,
-                                        content: comment,
-                                      })
-                                    }
-                                    className="btn btn-sm btn-primary pull-right"
-                                    type="button"
-                                  >
-                                    <i className="fa fa-pencil fa-fw"></i>{" "}
-                                    {"Share"}
-                                  </button>
-                                </div>
-                              </div>
-                            </div>
-                            <div
-                              className="panel"
-                              style={{
-                                backgroundColor: "grey",
-                                borderRadius: "10px",
-                                border: "1px solid white",
-                              }}
-                            >
-                              <div className="panel-body">
-                                {/* <!-- Newsfeed Content -->
-      <!--===================================================--> */}
-                                {store.commentBody &&
-                                  store.commentBody.map((book, index) => {
-                                    if (book.google_books_id) {
-                                      return (
-                                        <Comments commentContent={comment} />
-                                      );
-                                    }
-                                  })}
-                              </div>
-                            </div>
-                          </div>
+                {/* {aqui termina la info del libro modal} */}
+                <div className="container bootdey">
+                  <div className="col-md-12 bootstrap snippets">
+                    <div className="panel">
+                      <div className="panel-body">
+                        <textarea
+                          value={comment}
+                          onChange={(e) => setComment(e.target.value)}
+                          className="form-control"
+                          rows="2"
+                          placeholder="What are you thinking?"
+                        ></textarea>
+                        <div className="mar-top clearfix">
+                          <button
+                            onClick={() =>
+                              actions.addComment({
+                                google_books_id: props.google_books_id,
+                                // book_id: commentBody.book_id,
+                                content: comment,
+                              })
+                            }
+                            className="btn btn-sm btn-primary pull-right"
+                            type="button"
+                          >
+                            <i className="fa fa-pencil fa-fw"></i> {"Share"}
+                          </button>
                         </div>
+                      </div>
+                    </div>
+                    <div
+                      className="panel"
+                      style={{
+                        backgroundColor: "grey",
+                        borderRadius: "10px",
+                        border: "1px solid white",
+                      }}
+                    >
+                      <div className="panel-body">
+                        {/* <!-- Newsfeed Content -->
+      <!--===================================================--> */}
+                        {store.commentBody &&
+                          store.commentBody.map((book, index) => {
+                            if (book.google_books_id) {
+                              return <Comments commentContent={comment} />;
+                            }
+                          })}
                       </div>
                     </div>
                   </div>
