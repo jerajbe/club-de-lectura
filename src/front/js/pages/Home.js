807affd9a886f142c65f02ba42aa786ab6f42045
jerajbe <jerajbe@gmail.com>
2022-08-08 23:45:39 +0000

    se arrgla el home estilo

--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -20,38 +20,64 @@ export const Home = () => {
 
   return (
     <>
-      <div className="header">
-        <div className="row1">
-          <h1 style={{ color: "white" }}>Welcome to Lectur's Club</h1>
-        </div>
-        <div className="row2 card slider-frame ">
-          <ul>
-          <li> <img className="card-img-top img--card" src="https://p1.pxfuel.com/preview/781/453/305/book-hands-reading-man.jpg"></img></li>
-          <li> <img className="card-img-top img--card" src="https://mh-1-banco-de-imagen.panthermedia.net/media/media_detail/0025000000/25802000/~grupo-de-personas-leyendo-libros-sagrados_25802815_detail.jpg"></img></li>
-          <li> <img className="card-img-top img--card" src="https://media.thegospelcoalition.org/wp-content/uploads/sites/4/2020/02/31160000/Personas-leyendo-la-Biblia.jpg"></img></li>
-          </ul>
-          <h2 style={{ color: "black" }}>Find your Book</h2>
-          <div className="inputSearch">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Enter your Book Name"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyDown={searchBook}
-            />
-            <button onClick={() => actions.googleBooks(search)} className="btn btn-outline-secondary" type="button" id="button-addon2">
-              <i className="fas fa-search"></i>
-            </button>
+      <div className="fondoheader">
+        <div className="header container d-flex h-75 mt-2 mb-4 justify-content-between">
+          <div className="row1">
+            <h1 style={{ color: "white" }}>Welcome to Lectur's Club</h1>
+          </div>
+          <div className="row2 ms-2 card slider-frame w-25">
+            <ul>
+              <li>
+                {" "}
+                <img
+                  className="card-img-top img--card"
+                  src="https://p1.pxfuel.com/preview/781/453/305/book-hands-reading-man.jpg"
+                ></img>
+              </li>
+              <li>
+                {" "}
+                <img
+                  className="card-img-top img--card"
+                  src="https://mh-1-banco-de-imagen.panthermedia.net/media/media_detail/0025000000/25802000/~grupo-de-personas-leyendo-libros-sagrados_25802815_detail.jpg"
+                ></img>
+              </li>
+              <li>
+                {" "}
+                <img
+                  className="card-img-top img--card"
+                  src="https://media.thegospelcoalition.org/wp-content/uploads/sites/4/2020/02/31160000/Personas-leyendo-la-Biblia.jpg"
+                ></img>
+              </li>
+            </ul>
+            <h2 style={{ color: "black" }}>Find your Book</h2>
+            <div className="inputSearch">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Enter your Book Name"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={searchBook}
+              />
+              <button
+                onClick={() => actions.googleBooks(search)}
+                className="btn btn-outline-secondary"
+                type="button"
+                id="button-addon2"
+              >
+                <i className="fas fa-search"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="m-auto container">
+
+      <div className="container mb-3 mt-3 d-flex justify-content-center align-items-center">
         {store.searchGoogle.length <= 0 ? (
-          <div className="carouselHome">
+          <div className="carouselHome me-4">
             <div
               id="carouselExampleCaptions"
-              className="carousel carousel-dark slide d-flex justify-content-center w-75"
+              className="carousel carousel-dark slide d-flex justify-content-center w-100"
               data-bs-ride="false"
             >
               {/* {} */}
@@ -79,30 +105,6 @@ export const Home = () => {
                     })}
                 </div>
               </div>
-              {/* {} */}
-              {/* {} */}
-              {/* <div className="carousel-inner d-flex justify-content-center h-25 w-50">
-              {store.BestBooksYear &&
-                store.BestBooksYear.map((book, index) => {
-                  let thumbnail =
-                    book.volumeInfo.imageLinks &&
-                    book.volumeInfo.imageLinks.smallThumbnail;
-                  return (
-                    <div
-                      className={`carousel-item ${
-                        index < 1 ? "active" : ""
-                      } w-100 `}
-                      key={index}
-                    >
-                      <CarouselHome
-                        cover={thumbnail}
-                        key={index}
-                        name={book.volumeInfo.title}
-                      />
-                    </div>
-                  );
-                })}
-            </div> */}
               <button
                 className="carousel-control-prev"
                 type="button"
@@ -156,8 +158,10 @@ export const Home = () => {
               }
             })}
         </div>
+        <h2 style={{ color: "white" }} className="ms-4">
+          Best books of 2021
+        </h2>
       </div>
     </>
   );
 };
-
